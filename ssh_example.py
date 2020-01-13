1c986818fb3b826b64c92ce819fcb43d7d6a0558
Kamil Miekus <miekus@yahoo.co.uk>
2020-01-13 23:49:25 +0000

    updates to funciton arguments parsing and extra checks

--- ssh_example.py
+++ ssh_example.py
@@ -13,6 +13,25 @@ import getpass
 import subprocess
 import pexpect
 
+
+def checkInput():
+    while True:
+        IP = input('IP: ')
+        USERNAME = input('Username: ')
+        PASSWORD = getpass.getpass()
+        try:
+            if not all([IP, USERNAME, PASSWORD ]):
+                raise ValueError
+        except ValueError:
+            print("Please enter an appropriate values!")
+        else:
+            IP_check = IP.split('.')
+            if (len(IP_check) == 4) and (1 <= int(IP_check[0]) <= 223) and (int(IP_check[0]) != 127) and (int(IP_check[0]) != 169 or int(IP_check[1]) != 254) and (0 <= int(IP_check[1]) <= 255 and 0 <= int(IP_check[2]) <= 255 and 0 <= int(IP_check[3]) <= 255): 
+                return IP, USERNAME, PASSWORD
+            else:
+                print("Wrong IP format, try again...")
+                continue
+
 def newlines():
     "Function to pretty add number of spearators"
     print('\n')
@@ -25,34 +44,39 @@ def newlines():
     print('\n')
 
 
-USERNAME = input('Username: ')
-PASSWORD = getpass.getpass()
 
-CHILD = pexpect.spawn('ssh -c aes256-cbc -l %s 192.168.0.254' % USERNAME)
-CHILD.logfile_read = sys.stdout.buffer
+def execute(*args):
+    CHILD = pexpect.spawn('ssh -c aes256-cbc %s -l %s' % (first, second))
+    CHILD.logfile_read = sys.stdout.buffer
 
-CHILD.expect('.*assword:.*')
-CHILD.sendline(PASSWORD)
-CHILD.expect('.*#.*')
+    CHILD.expect('.*assword:.*')
+    CHILD.sendline(third)
+    CHILD.expect('.*#.*')
 
-newlines()
-CHILD.sendline('terminal length 0')
-CHILD.expect('.*#.*')
+    newlines()
+    CHILD.sendline('terminal length 0')
+    CHILD.expect('.*#.*')
 
-newlines()
-CHILD.sendline('show version')
-CHILD.expect('.*#.*')
+    newlines()
+    CHILD.sendline('show version')
+    CHILD.expect('.*#.*')
 
-newlines()
-CHILD.sendline('show ip int brief')
-CHILD.expect('.*#.*')
+    newlines()
+    CHILD.sendline('show ip int brief')
+    CHILD.expect('.*#.*')
 
-newlines()
-CHILD.sendline('show process cpu history')
-CHILD.expect('.*#.*')
+    newlines()
+    CHILD.sendline('show process cpu history')
+    CHILD.expect('.*#.*')
 
-newlines()
-subprocess.call("ls -la", shell=True)
-newlines()
+    newlines()
+    subprocess.call("ls -la", shell=True)
+    newlines()
 
-CHILD.close()
+    CHILD.close()
+ 
+
+if __name__ == "__main__":
+
+    first, second, third = checkInput()
+    execute(first, second, third)
